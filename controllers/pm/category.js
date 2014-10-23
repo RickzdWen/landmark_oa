f8907b5987fbc54d0dc2b9ab41ead64a33f8c71a
RickzdWen <zhengdwen@qq.com>
2014-10-23 23:46:48 +0800

    category

--- controllers/pm/category.js
+++ controllers/pm/category.js
@@ -5,12 +5,25 @@
 var express = require('express');
 var router = express.Router();
 var ProductCategoryModel = require(ROOT_PATH + '/models/ProductCategoryModel');
+var pager = require(ROOT_PATH + '/libs/pager');
 var CommonError = require(ROOT_PATH + '/libs/errors/CommonError');
 
 router.get('/categories', function(req, res, next){
     try {
-        ProductCategoryModel.getInstance().getByPage().then(function(ret){
-            res.json(ret);
+        var page = req.query.page || 1;
+        var of = req.query.of || '';
+        if (!of) {
+            res._view = 'pm/categories';
+        }
+        ProductCategoryModel.getInstance().getByPage(page).then(function(ret){
+            var pageInfo = pager.getPagerInfo(ret.page);
+            ret.pageInfo = pageInfo;
+            res.doc = ret;
+            if (res._view) {
+                res.render(res._view, res);
+            } else {
+                res.json(res.doc);
+            }
         }, function(err){
             next(err);
         });
