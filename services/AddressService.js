8a51d77069c9bbcaeaf1af6c9890f74e76e9de01
RickzdWen <zhengdwen@qq.com>
2015-03-31 18:36:13 +0800

    address

--- services/AddressService.js
+++ services/AddressService.js
@@ -8,3 +8,16 @@ var CommonError = require(ROOT_PATH + '/libs/errors/CommonError');
 exports.getCurrentUsedAddress = function(uid) {
     return AddressModel.getInstance().getOneAddressByUid(uid);
 };
+
+exports.updateCurrentUsedAddress = function(data, uid, id) {
+    if (!uid) {
+        throw new CommonError('', 50002);
+    }
+    data = data || {};
+    if (id) {
+        return AddressModel.getInstance().update(data, 'id=? AND uid=?', [id, uid]);
+    } else {
+        data.uid = uid;
+        return AddressModel.getInstance().addNewOne(data);
+    }
+};
