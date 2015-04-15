52a103fb81f955def576f10a87b5e637eb678ecc
RickzdWen <zhengdwen@qq.com>
2015-04-16 00:42:34 +0800

    paypal service

---
+++ services/PaypalService.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2015/4/15.
+ */
+
+var paypal_sdk = require('paypal-rest-sdk');
+var q = require('q');
+
+paypal_sdk.configure({
+    'host': 'api.sandbox.paypal.com',
+    'client_id': 'ATPW5jxAISD1e_RQ5c9ngoZjDI6QoMJujlgt8zvC0yawLCkF35xzKCbPWSM_gwK7Y8b0vYGcGXDRg2iX',
+    'client_secret': 'EDA_tSLZRaR6mRmGsUEEgaMP_m0DDvTOuryaabFkyf0-CviVL6_hnm_DO0ntGjqsoXTtp8NpPD6zSGto' });
+
+exports.createPayment = function(data) {
+    var details = {
+        intent : 'sale',
+        payer : {
+            payment_method : data.method || 'paypal'
+        },
+        transactions : [{
+            amount : data.amount,
+            description : data.desc || 'landmark shop payment'
+        }],
+        redirect_urls : {
+            return_url : data.returnUrl || 'http://localhost:3000/account/paypal/return',
+            cancel_url : data.cancelUrl || 'http://localhost:3000/account/paypal/return_cancel'
+        }
+    };
+    var delay = q.defer();
+    paypal_sdk.payment.create(details, function(error, payment){
+        if (error) {
+            delay.reject(error);
+        } else {
+            delay.resolve(payment);
+        }
+    });
+    return delay.promise;
+};
